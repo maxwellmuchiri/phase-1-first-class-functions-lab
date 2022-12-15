aa3221d0e26e3e96cb10e94a6cf230311c29bc95
maxwellmuchiri <muchirimaxwel@gmail.com>
2022-12-15 14:15:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    const newDriver= drivers.slice(0,2);
+    return newDriver;
+}
+function returnLastTwoDrivers(drivers){
+    const newDriver = drivers.slice(2)
+    return newDriver;
+}
+
+
+    const selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+    function createFareMultiplier(value){
+        return((fare)=>(fare*value))
+
+    }
+
+    const fareDoubler = createFareMultiplier('2');
+    const fareTripler = createFareMultiplier('3');
+
+    function selectDifferentDrivers(arrayOfDrivers, callback){
+        return callback(arrayOfDrivers);
+    }
